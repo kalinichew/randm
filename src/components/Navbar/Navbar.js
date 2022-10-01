fc95bc0c90c601013926b33cfba49853cde12e4e
Vladimir <70281965+kalinichew@users.noreply.github.com>
2022-10-02 00:54:41 +0300

    Initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import "../../App.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+      <div className="container">
+        <Link to="/" className="navbar-brand fs-3 ubuntu">
+          Rick & Morty <span className="text-primary">Энциклопедия</span>
+        </Link>
+        <style jsx>{`
+          button[aria-expanded="false"] > .close {
+            display: none;
+          }
+          button[aria-expanded="true"] > .open {
+            display: none;
+          }
+        `}</style>
+        <button
+          className="navbar-toggle border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigator"
+        >
+          <span class="fas fa-bars open text-dark"></span>
+          <span class="fas fa-times close text-dark"></span>
+        </button>
+        <div
+          className="collaplse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav fs-5">
+            <NavLink to="/" className="nav-link">
+              Персонажи
+            </NavLink>
+            <NavLink to="/episodes" className="nav-link">
+              Эпизоды
+            </NavLink>
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              to="/location"
+            >
+              Локации
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
