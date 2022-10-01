8c50511d96146596fef76681bc14c9d44cab1844
Vladimir <70281965+kalinichew@users.noreply.github.com>
2022-10-02 01:41:58 +0300

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Card.module.scss";
-import CardDetails from "./CardDetails";
+
 
 const Card = ({ page, results }) => {
   let display;
